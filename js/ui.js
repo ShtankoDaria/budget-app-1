391625b30572eeb5343cbbbcce9e53d91a983172
Francisco Fernández <ferrycosv@gmail.com>
2020-04-17 13:42:26 +0200

    Expense form to process and calculate expenses provided by the user

--- js/ui.js
+++ js/ui.js
@@ -21,8 +21,7 @@ class UI {
       this.budgetFeedback.classList.add("showItem");
       this.budgetFeedback.innerHTML =
         "<p>value cannot be empty or negative</p>";
-      setTimeout(() => this.budgetFeedback.classList.remove("showItem"), 4000)
-        .bind;
+      setTimeout(() => this.budgetFeedback.classList.remove("showItem"), 4000);
     } else {
       this.budgetAmount.textContent = val;
       this.budgetInput.value = "";
@@ -34,19 +33,67 @@ class UI {
     const total = parseInt(this.budgetAmount.textContent) - expense;
     this.balanceAmount.textContent = total;
     if (total > 0) {
-        this.balance.classList.remove("showBlack","showRed");
-        this.balance.classList.add("showGreen");
+      this.balance.classList.remove("showBlack", "showRed");
+      this.balance.classList.add("showGreen");
     } else if (total < 0) {
-        this.balance.classList.remove("showBlack","showGreen");
-        this.balance.classList.add("showRed");
+      this.balance.classList.remove("showBlack", "showGreen");
+      this.balance.classList.add("showRed");
     } else {
-        this.balance.classList.remove("showRed","showGreen");
-        this.balance.classList.add("showBlack");
+      this.balance.classList.remove("showRed", "showGreen");
+      this.balance.classList.add("showBlack");
     }
   }
+  submitExpenseForm() {
+    const expenseval = this.expenseInput.value;
+    const amountval = this.amountInput.value;
+    if (expenseval === "" || amountval === "" || amountval < 0) {
+      this.expenseFeedback.classList.add("showItem");
+      this.expenseFeedback.innerHTML =
+        "<p>value cannot be empty or negative</p>";
+      setTimeout(() => this.expenseFeedback.classList.remove("showItem"), 4000);
+    } else {
+      let amount = parseInt(amountval);
+      this.expenseInput.value = "";
+      this.amountInput.value = "";
+      let expense = {
+        id: this.itemID,
+        title: expenseval,
+        amount: amount,
+      };
+      this.itemID++;
+      this.itemList.push(expense);
+      this.addExpense(expense);
+      this.showBalance();
+    }
+  }
+  addExpense(expense) {
+    const div = document.createElement("div");
+    div.classList.add("expense");
+    div.innerHTML = `
+      <div class="expense-item d-flex justify-content-between align-items-baseline">
+
+      <h6 class="expense-title mb-0 text-uppercase list-item">- ${expense.title}</h6>
+      <h5 class="expense-amount mb-0 list-item">${expense.amount}</h5>
+
+      <div class="expense-icons list-item">
+
+       <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+        <i class="fas fa-edit"></i>
+       </a>
+       <a href="#" class="delete-icon" data-id="${expense.id}">
+        <i class="fas fa-trash"></i>
+       </a>
+      </div>
+     </div>      
+      `;
+    this.expenseList.appendChild(div);
+  }
   totalExpense() {
-    //let total = this.totalExpense();
-    let total = 400; 
+    let total = 0;
+    if (this.itemList.length > 0) {
+      total = this.itemList.reduce((sum, item) => sum + item.amount, 0);
+    }
+    this.expenseAmount.textContent = total;
     return total;
   }
 }
